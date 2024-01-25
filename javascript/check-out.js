4b8102d157f11581c7fed94afd775dfc5f5c51ae
Ilham Dwi Kurniawan <dkilham51@gmail.com>
2024-01-25 11:29:28 +0700

    Update check-out.js

--- javascript/check-out.js
+++ javascript/check-out.js
@@ -1,26 +1,31 @@
-
-// hamburger button
-
 var leftMenu = document.getElementById("left-menu");
+var leftNavContent = document.getElementById("left-nav-content");
 var navToggle = document.getElementById("nav-toggle");
 var navClose = document.getElementById("nav-close");
 
 function toggleMenu() {
     leftMenu.classList.toggle("show-menu");
 
+    // Apply styles to elements with the class "li-left" when the menu is open
+    var liLeftElements = document.getElementsByClassName("li-left");
+    for (var i = 0; i < liLeftElements.length; i++) {
+        liLeftElements[i].style.marginRight = leftMenu.classList.contains("show-menu") ? "0px" : "";
+    }
+
     if (leftMenu.classList.contains("show-menu")) {
         navToggle.style.display = "none";
         navClose.style.display = "block";
+        navClose.style.left = "250px";
+        navClose.style.transition = "0.75s";
     } else {
         navToggle.style.display = "block";
-        navClose.style.display = "none";
+        navClose.style.left = "-250px";
     }
 }
 
 function updateNavVisibility() {
     var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     navToggle.style.display = windowWidth > 767 ? "none" : "block";
-    navClose.style.display = "none";
 }
 
 updateNavVisibility();
@@ -36,31 +41,6 @@ window.addEventListener("scroll", function () {
     }
 });
 
-
-
-
-var leftMenu = document.getElementById("left-menu");
-var navToggle = document.getElementById("nav-toggle");
-var navClose = document.getElementById("nav-close");
-
-function toggleMenu() {
-    leftMenu.classList.toggle("show-menu");
-    navToggle.style.display = navToggle.style.display === "none" ? "block" : "none";
-    navClose.style.display = navClose.style.display === "none" ? "block" : "none";
-}
-
-// Set display none for nav-close when the page loads
-navClose.style.display = "none";
-
-// Set up click event listeners for nav-toggle and nav-close
-navToggle.addEventListener("click", toggleMenu);
-navClose.addEventListener("click", toggleMenu);
-
-// Set up resize event listener to handle screen size changes
-window.addEventListener("resize", function () {
-    var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    navToggle.style.display = windowWidth > 767 ? "none" : "block";
-});
 
 // popup new address
 
